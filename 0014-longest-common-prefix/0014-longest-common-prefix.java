9ba775170eeb5c8be98136349005f73c361d7cb5
Navendu-creator <84001342+Navendu-creator@users.noreply.github.com>
2024-03-20 18:14:00 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (65.37%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+       if(strs.length==0)
+       return "";
+       String p=strs[0];
+       for(int i=0;i<strs.length;i++)
+       {
+        while(strs[i].indexOf(p)!=0)
+        {
+            p=p.substring(0,p.length()-1);
+        }
+       }
+       return p;
+    }
+}
